f7d4f73b0d7036eeefa6889b9b3ba5eae63eaeca
hazelvaq <hvaquero98@gmail.com>
2024-08-26 16:25:59 -0700

    recreate app

--- global.R
+++ global.R
@@ -1,42 +1,24 @@
-# Check for Auth Tokens and setup, you can change these to test the triggering
-# of functions without removing the files.
-translate <- file.exists("www/googletranslate.html")
-
-#remotes::install_github("wincowgerDEV/OpenSpecy-package@vignettes")
-
-# Libraries ----
 library(shiny)
-library(shinyjs)
-library(shinyWidgets)
-library(dplyr)
-library(plotly)
+library(bslib)
+library(caTools)
 library(data.table)
-library(DT)
-library(digest)
-library(OpenSpecy)
-library(bs4Dash)
-library(ggplot2)
+library(glmnet)
+library(hyperSpec)
+library(mmand)
+library(plotly)
+library(signal)
+webr::install("OpenSpecyWebr", repos = "https://moore-institute-4-plastic-pollution-res.github.io/OpenSpecyWebr/")
+library(OpenSpecyWebr)
 
 # Load all data ----
 load_data <- function() {
   data("raman_hdpe")
   
-  testdata <-  data.table(wavenumber = raman_hdpe$wavenumber, 
-                 intensity = raman_hdpe$spectra$intensity)
+  load_data <- function() {
+    data("raman_hdpe")
+    testdata <- data.table(wavenumber = raman_hdpe$wavenumber, 
+                           intensity = raman_hdpe$spectra$intensity)
+    return(testdata)
+  }
   
-  # Inject variables into the parent environment
-  invisible(list2env(as.list(environment()), parent.frame()))
 }
-
-# Name keys for human readable column names ----
-
-version <- paste0("Open Specy v", packageVersion("OpenSpecy"))
-citation <- HTML(
-  "Cowger W, Steinmetz Z, Gray A, Munno K, Lynch J, Hapich H, Primpke S, De
-  Frond H, Rochman C, Herodotou O (2021). “Microplastic Spectral
-  Classification Needs an Open Source Community: Open Specy to the Rescue!”
-  <i>Analytical Chemistry</i>, <b>93</b>(21), 7543–7548. doi:
-  <a href='https://doi.org/10.1021/acs.analchem.1c00123'>10.1021/acs.analchem.1c00123</a>."
-)
-
-
